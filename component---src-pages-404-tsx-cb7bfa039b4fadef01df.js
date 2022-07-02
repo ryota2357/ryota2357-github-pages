a2d654acb16a4e04af80f34b7c1dbe4ec42d55b1
ryota2357 <ryota2357@users.noreply.github.com>
2022-07-02 10:16:09 +0000

    deploy: ryota2357/ryota2357-github-pages@e0fc871e3f8a90825a419ec8d6710fa447080304

---
+++ component---src-pages-404-tsx-cb7bfa039b4fadef01df.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkryota2357=self.webpackChunkryota2357||[]).push([[218],{7735:function(n,s,e){e.r(s);var t=e(4290),r=e(5893);s.default=function(){return(0,r.jsxs)(t.Ar,{id:"not-found-page",children:[(0,r.jsx)(t.pQ,{title:"404: Not Found"}),(0,r.jsxs)("h1",{children:[(0,r.jsx)("span",{children:"404"}),(0,r.jsx)("span",{children:"Not Found"})]})]})}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-cb7bfa039b4fadef01df.js.map
